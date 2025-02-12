af2fd878dde65222dcbd6119c28efae40783a3d4
migueltato <migueltato@gmail.com>
2025-02-12 17:54:09 +0100

    second commit

---
+++ LittleLemonDM.sql
@@ -0,0 +1,226 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema little_lemon
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema little_lemon
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `little_lemon` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `little_lemon` ;
+
+-- -----------------------------------------------------
+-- Table `little_lemon`.`Employees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `little_lemon`.`Employees` (
+  `EmployeeID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(100) NULL DEFAULT NULL,
+  `Role` VARCHAR(100) NULL DEFAULT NULL,
+  `Address` VARCHAR(200) NULL DEFAULT NULL,
+  `Contact_Number` INT NULL DEFAULT NULL,
+  `Email` VARCHAR(100) NULL DEFAULT NULL,
+  `Annual_Salary` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`EmployeeID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `little_lemon`.`Customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `little_lemon`.`Customers` (
+  `CustomerID` INT NOT NULL,
+  `FirstName` VARCHAR(100) NOT NULL,
+  `LastName` VARCHAR(100) NOT NULL,
+  `PhoneNumber` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`CustomerID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `little_lemon`.`Bookings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `little_lemon`.`Bookings` (
+  `BookingID` INT NOT NULL AUTO_INCREMENT,
+  `TableNo` INT NULL DEFAULT NULL,
+  `GuestFirstName` VARCHAR(100) NOT NULL,
+  `GuestLastName` VARCHAR(100) NOT NULL,
+  `BookingSlot` TIME NOT NULL,
+  `EmployeeID` INT NULL DEFAULT NULL,
+  `CustomerID` INT NULL,
+  `BookingDate` DATE NOT NULL,
+  PRIMARY KEY (`BookingID`),
+  INDEX `EmployeeID_idx` (`EmployeeID` ASC) VISIBLE,
+  INDEX `CustomerID_idx` (`CustomerID` ASC) VISIBLE,
+  CONSTRAINT `EmployeeID`
+    FOREIGN KEY (`EmployeeID`)
+    REFERENCES `little_lemon`.`Employees` (`EmployeeID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `CustomerID`
+    FOREIGN KEY (`CustomerID`)
+    REFERENCES `little_lemon`.`Customers` (`CustomerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 10
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `little_lemon`.`MenuItems`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `little_lemon`.`MenuItems` (
+  `MenuItemID` INT NOT NULL AUTO_INCREMENT,
+  `MenuItemName` VARCHAR(100) NULL DEFAULT NULL,
+  `MenuItemType` DECIMAL(5,2) NULL DEFAULT NULL,
+  `MenuItemPrice` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`MenuItemID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 18
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `little_lemon`.`Menus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `little_lemon`.`Menus` (
+  `MenuID` INT NOT NULL,
+  `ItemID` INT NOT NULL,
+  `Cuisine` VARCHAR(100) NULL DEFAULT NULL,
+  `MenuItemID` INT NULL,
+  `Starters` VARCHAR(100) NULL,
+  `Courses` VARCHAR(100) NULL,
+  `Drinks` VARCHAR(100) NULL,
+  `Deserts` VARCHAR(100) NULL,
+  PRIMARY KEY (`MenuID`),
+  INDEX `MenuItemID_idx` (`MenuItemID` ASC) VISIBLE,
+  CONSTRAINT `MenuItemID`
+    FOREIGN KEY (`MenuItemID`)
+    REFERENCES `little_lemon`.`MenuItems` (`MenuItemID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `little_lemon`.`Orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `little_lemon`.`Orders` (
+  `OrderID` INT NOT NULL,
+  `TableNo` INT NOT NULL,
+  `MenuID` INT NULL DEFAULT NULL,
+  `BookingID` INT NULL DEFAULT NULL,
+  `BillAmount` INT NULL DEFAULT NULL,
+  `Quantity` INT NULL DEFAULT NULL,
+  `OrderDate` DATE NULL,
+  `MenuID` INT NULL,
+  `CustomerID` INT NULL,
+  `TotalCost` DECIMAL(5,2) NULL,
+  PRIMARY KEY (`OrderID`),
+  INDEX `MenuID_idx` (`MenuID` ASC) VISIBLE,
+  INDEX `CustomerID_idx` (`CustomerID` ASC) VISIBLE,
+  CONSTRAINT `MenuID`
+    FOREIGN KEY (`MenuID`)
+    REFERENCES `little_lemon`.`Menus` (`MenuID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `CustomerID`
+    FOREIGN KEY (`CustomerID`)
+    REFERENCES `little_lemon`.`Customers` (`CustomerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `little_lemon`.`OrderDelivery`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `little_lemon`.`OrderDelivery` (
+  `OrderDeliveryID` INT NOT NULL,
+  `OrderDeliveryDate` DATE NULL,
+  `OrderDeliveryStatus` VARCHAR(100) NULL,
+  `OrderID` INT NULL,
+  PRIMARY KEY (`OrderDeliveryID`),
+  INDEX `OrderID_idx` (`OrderID` ASC) VISIBLE,
+  CONSTRAINT `OrderID`
+    FOREIGN KEY (`OrderID`)
+    REFERENCES `little_lemon`.`Orders` (`OrderID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `little_lemon` ;
+
+-- -----------------------------------------------------
+-- procedure BasicSalesReport
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `little_lemon`$$
+CREATE DEFINER=`newuser`@`localhost` PROCEDURE `BasicSalesReport`()
+BEGIN
+SELECT SUM(BillAmount) AS Total_sales,
+AVG(BillAmount) AS Average_sale,
+MIN(BillAmount) AS Min_bill_paid,
+MAX(BillAmount) AS Max_bill_paid
+FROM Orders;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure GuestStatus
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `little_lemon`$$
+CREATE DEFINER=`newuser`@`localhost` PROCEDURE `GuestStatus`()
+BEGIN
+SELECT CONCAT(GuestFirstName, ' ', GuestLastName) AS GuestFullName,
+CASE
+WHEN Role in ('Manager','Assistant Manager') THEN 'Ready to pay'
+WHEN Role in ('Head Chef') THEN 'Ready to serve'
+WHEN Role in ('Assistant Chef') THEN 'Preparing Order'
+WHEN Role in ('Head Waiter') THEN 'Order served'
+ELSE "Pending"
+END AS Status
+FROM Bookings
+LEFT JOIN Employees
+ON Bookings.EmployeeID = Employees.EmployeeID;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure PeakHours
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `little_lemon`$$
+CREATE DEFINER=`newuser`@`localhost` PROCEDURE `PeakHours`()
+BEGIN
+SELECT HOUR(BookingSlot) AS booking_hour, COUNT(BookingID) AS number_of_bookings
+FROM Bookings
+GROUP BY booking_hour
+ORDER BY number_of_bookings DESC;
+END$$
+
+DELIMITER ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
